c25f2fd429f48e9aeffb81b43cdfa40db58b6e0b
nassorc <macrossan17@gmail.com>
2024-09-09 05:27:49 -0700

    Add camera smoothing when player changes directions

--- features/camera.go
+++ features/camera.go
@@ -2,15 +2,17 @@ package features
 
 import (
 	"fmt"
-  "math"
 	"image/color"
+	"math"
 	"platformer/components"
 	"platformer/config"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/vector"
 	dmath "github.com/nassorc/go-codebase/lib/math"
+	"github.com/yohamta/donburi"
 	"github.com/yohamta/donburi/ecs"
+	"github.com/yohamta/donburi/filter"
 )
 
 type Camera struct {
@@ -42,15 +44,23 @@ type CameraFollowPlayer struct {
   // Different from camera.Zoom
   // used to implement smooth zooming
   Zoom float64
+  query *donburi.Query
 }
 
 func NewCameraFollowPlayer(camera *Camera) CameraFollowPlayer {
-  return CameraFollowPlayer{ camera, camera.Zoom }
+  query := donburi.NewQuery(filter.Contains(components.Player))
+
+  return CameraFollowPlayer{ camera, camera.Zoom, query }
+}
+
+func Lerp(x, y, val float64) float64 {
+  return (y-x) * val + x
 }
 
 func (c *CameraFollowPlayer) Update(ecs *ecs.ECS) {
-  player := components.MustFindPlayer(ecs.World)
-  obj := components.Object.Get(player)
+  entry, _ := c.query.First(ecs.World)
+  player := components.Player.Get(entry)
+  obj := components.Object.Get(entry)
 
   // make camera target player position
   x := float32(obj.X)
@@ -58,10 +68,35 @@ func (c *CameraFollowPlayer) Update(ecs *ecs.ECS) {
   sw := c.camera.Viewport.Max.X / float32(c.camera.Zoom)
 	sh := c.camera.Viewport.Max.Y / float32(c.camera.Zoom)
 
-	x -= sw / 2
-	y -= sh / 2
+  // subtract player's object half size to make origin center
+	x -= sw / 2 - 8
+	y -= sh / 2 - 10
 
-  c.camera.Position.X = x
+  trapOffset := 0
+
+  // camera trap
+  if player.FacingRight {
+    trapOffset = 25
+  } else {
+    trapOffset = -25
+  }
+
+  // Lerp(c.camera.Position.X, x+float32(old), 0.5)
+
+  // follow player 
+  // c.camera.Position.X = float32(Lerp(float64(x), float64(x + float32(trapOffset)), 0.1))
+
+  // finalX := playerPos + trapoffset
+  // cameraX = Lerp(cameraX, finalX, 0.5)
+
+  // maybe twice the trapoffset
+  // newFinalX = float32(c.camera.Position.X + float32(trapOffset))
+
+  finalX := float32(obj.X + float64(trapOffset))
+  cameraX := Lerp(float64(c.camera.Position.X), float64(finalX), 0.05)
+
+  // c.camera.Position.X = x + float32(trapOffset)
+  c.camera.Position.X = float32(cameraX)
   c.camera.Position.Y = y
 
   // bound camera
@@ -86,4 +121,22 @@ func (c *CameraFollowPlayer) Update(ecs *ecs.ECS) {
 		c.Zoom -= 1
 		c.camera.Zoom = math.Pow(1.01, c.Zoom)
 	}
+}
+
+type DebugCamera struct {
+  camera *Camera
+}
+
+func NewDebugCamera(camera *Camera) DebugCamera {
+  return DebugCamera{ camera }
+}
+
+func (c *DebugCamera) Draw(ecs *ecs.ECS, screen *ebiten.Image) {
+  settings := components.MustFindSettings(ecs.World)
+  if settings.Debug {
+    center := c.camera.Viewport.Center()
+    drawColor := color.RGBA{255, 20, 20, 255}
+    fmt.Println("drawing camera @", center.X, center.Y)
+    vector.DrawFilledCircle(screen, center.X, center.Y, 4, drawColor, false)
+  }
 }
